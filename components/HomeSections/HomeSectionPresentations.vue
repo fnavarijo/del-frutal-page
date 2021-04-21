e4419cbb00d8fabc1976b8e5625c784a49106275
Fernando Navarijo <fernandonavarijo1996@gmail.com>
2021-04-21 00:19:35 -0600

    adding presentations to section

--- components/HomeSections/HomeSectionPresentations.vue
+++ components/HomeSections/HomeSectionPresentations.vue
@@ -8,6 +8,7 @@
       backgroundRepeat: 'no-repeat',
     }"
     inner-container-class="w-full text-normal-brown-500"
+    max-content=""
   >
     <div class="grid grid-cols-3">
       <div>
@@ -21,6 +22,14 @@
           Tiene un sabor y calidad incomparable, un tono dorado oscuro y su
           espuma clara y estable.
         </p>
+        <div class="flex items-end">
+          <PresentationLabel
+            v-for="(presentation, index) in presentationsLabels"
+            :key="index"
+            v-bind="presentation"
+            class="mx-2"
+          />
+        </div>
       </div>
       <div></div>
       <div>
@@ -72,5 +81,36 @@
 import Vue from 'vue';
 export default Vue.extend({
   name: 'HomeSectionPresentations',
+  computed: {
+    presentationsLabels() {
+      return [
+        {
+          label: '12 FL. OZ',
+          imageUrl:
+            'https://res.cloudinary.com/dxsslqd7f/image/upload/v1618258523/FAMOSA/Producto/12_oz_bakrr8.svg',
+        },
+        {
+          label: '16 FL. OZ',
+          imageUrl:
+            'https://res.cloudinary.com/dxsslqd7f/image/upload/v1618258566/FAMOSA/Producto/16_oz_nz5mze.svg',
+        },
+        {
+          label: '24 FL. OZ',
+          imageUrl:
+            'https://res.cloudinary.com/dxsslqd7f/image/upload/v1618258565/FAMOSA/Producto/24oz_e4zqtl.svg',
+        },
+        {
+          label: '12 FL. OZ',
+          imageUrl:
+            'https://res.cloudinary.com/dxsslqd7f/image/upload/v1618258576/FAMOSA/Producto/12oz_d1t7bg.svg',
+        },
+        {
+          label: '32 FL. OZ',
+          imageUrl:
+            'https://res.cloudinary.com/dxsslqd7f/image/upload/v1618258576/FAMOSA/Producto/32oz_i7v8vf.svg',
+        },
+      ];
+    },
+  },
 });
 </script>
---
+++ components/Presentation/PresentationLabel.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="flex flex-col items-center">
+    <img :src="imageUrl" class="w-8" alt="" /><span class="text-sm">{{
+      label
+    }}</span>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+export default Vue.extend({
+  name: 'PresentationLabel',
+  props: {
+    imageUrl: {
+      type: String,
+      required: true,
+    },
+    label: {
+      type: String,
+      required: true,
+    },
+    isActive: {
+      type: Boolean,
+      default: false,
+    },
+  },
+});
+</script>
